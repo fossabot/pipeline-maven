f989f8a12eb9f0ff9b134752f1872c7985d853f6
Biswajyoti Deb <bjdeb1986@gmail.com>
2023-07-24 14:24:45 -0400

    Fixes workspace path for job execution

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,6 @@ echo "****************************"
 echo "** Building jar ***"
 echo "****************************"
 
-docker run --rm -it -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-17-alpine "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -it -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-17-alpine "$@"
