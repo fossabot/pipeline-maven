e7130bdf3cefb688424fccdf928553ec47cdf92d
Biswajyoti Deb <bjdeb1986@gmail.com>
2023-07-24 14:58:18 -0400

    corrected cert file path

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo "!Abcd@123" >> /tmp/.auth
 
-scp -i /home/jenkins/prod /tmp/.auth prod-user@192.168.56.106:/tmp/.auth
-scp -i /home/jenkins/prod ./jenkins/deploy/publish.sh prod-user@192.168.56.106:/tmp/publish.sh
-ssh -i /home/jenkins/prod prod-user@192.168.56.106 "/tmp/publish.sh"
+scp -i /opt/prod /tmp/.auth prod-user@192.168.56.106:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@192.168.56.106:/tmp/publish.sh
+ssh -i /opt/prod prod-user@192.168.56.106 "/tmp/publish.sh"
