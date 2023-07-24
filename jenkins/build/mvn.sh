c5069f40f2db75f26e6f525407cb4fae8efe520f
Biswajyoti Deb <bjdeb1986@gmail.com>
2023-07-24 14:42:45 -0400

    tty issue

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "****************************"
 
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -it -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-17-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-17-alpine "$@"
